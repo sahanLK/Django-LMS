d8a1377ee161cebc98c61edc7dd5c2a3c30954cc
Sahan <kksahan78@gmail.com>
2023-05-11 23:24:21 +0530

    nothing apecial

--- django_LMS/settings.py
+++ django_LMS/settings.py
@@ -144,8 +144,8 @@ AUTH_USER_MODEL = "users.CustomizedUser"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
