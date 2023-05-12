386dd3f2afb4ea01ddea8330720cae5bda10b2b6
Sahan <kksahan78@gmail.com>
2023-05-12 12:16:08 +0530

    done

--- django_LMS/settings.py
+++ django_LMS/settings.py
@@ -2,9 +2,6 @@ import os.path
 from pathlib import Path
 
 
-print("DB USER", os.environ.get('DB_USER'))
-print("DB PASSWORD", os.environ.get('DB_PWD'))
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -16,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dz0hc=@=j309k15yl5gm%@%q8s)3pk_(gx_2bg$e$x8)*p(5sm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # A variable for differ the settings
