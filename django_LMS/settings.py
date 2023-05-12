b31b7def8109ad9a06695608fcfcbd0faa1f3191
Sahan <kksahan78@gmail.com>
2023-05-12 16:26:20 +0530

    domain added

--- django_LMS/settings.py
+++ django_LMS/settings.py
@@ -13,13 +13,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dz0hc=@=j309k15yl5gm%@%q8s)3pk_(gx_2bg$e$x8)*p(5sm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 # A variable for differ the settings
 PRODUCTION = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '143.110.241.4', 'ucrlms.online']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '143.110.241.4',
+    'ucrlms.online',
+    'www.ucrlms.online'
+]
 
 
 # Application definition
